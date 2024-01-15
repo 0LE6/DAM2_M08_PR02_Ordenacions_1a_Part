5f1e98d833060c9814845647d49727be1fbb93e9
Oleg <kharenkoleg@gmail.com>
2024-01-15 15:52:55 +0100

    Agregar archivos de proyecto.

---
+++ DAM2_M08_PR02_Ordenacions_1a_Part/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DAM2_M08_PR02_Ordenacions_1a_Part
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
