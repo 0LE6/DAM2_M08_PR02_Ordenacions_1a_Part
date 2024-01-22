57664461d140956bef1d5643d623daf6f2db7d74
Oleg <kharenkoleg@gmail.com>
2024-01-22 02:46:25 +0100

    <<

---
+++ DAM2_M08_PR02_Ordenacions_1a_Part/Model/Ordenaciones.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAM2_M08_PR02_Ordenacions_1a_Part.Model
+{
+    public class Ordenaciones
+    {
+
+    }
+}
